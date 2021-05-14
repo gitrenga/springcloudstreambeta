59b370af7ab5ef0453de5fcf8718d6abde063832
venkitr1 <Rengasamy.Venkittaraman2@unisys.com>
2021-05-14 10:26:50 +0530

    downloaded spring intializr project for spring cloud stream

---
+++ ndcorchestrator/src/main/java/com/unisys/trans/ndcorchestrator/NdcorchestratorApplication.java
@@ -0,0 +1,13 @@
+package com.unisys.trans.ndcorchestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NdcorchestratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NdcorchestratorApplication.class, args);
+	}
+
+}
